5e1fd6c3ad34e0fc9084addc694174e7d548ea95
yonggwan <choiyk1225@gmail.com>
2020-09-09 12:30:04 +0900

    add global type declaration - png extension

--- types/global.d.ts
+++ types/global.d.ts
@@ -18,6 +18,11 @@ declare module '*.gif' {
   export default content;
 }
 
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
 declare module '*.svg' {
   const content: string;
   export default content;
