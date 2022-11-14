38244898552deb18c9b7f499d44b86c2ce4dca2f
yonggwan <choiyk1225@gmail.com>
2022-11-14 22:49:35 +0900

    react version upgrade 16 to 18

--- src/compile-test.ts
+++ src/compile-test.ts
@@ -1,19 +1,1 @@
-// import * as React from 'react';
-
-// import { Hello } from '@/components/Paragraph';
-
-// const _radsr = require('asdads');
-
-// const asd = {
-//   asddas: 213231231,
-//   ad: 0 * 1,
-// };
-
-// alert(alert.bind(this));
-
-// const use = function use(...args) {};
-
-// const use2 = function (a: any = Number(1)) {
-//   alert;
-// };
 export default '';
